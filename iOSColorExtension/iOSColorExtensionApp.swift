c602f5b02143b21c73d168d3a450e6e44214cead
VirajTank <virajtank98@gmail.com>
2023-01-26 20:51:52 +0530

    Initial Commit

---
+++ iOSColorExtension/iOSColorExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSColorExtensionApp.swift
+//  iOSColorExtension
+//
+//  Created by Viraj Tank on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSColorExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
